d047a9a45765ceaaaa4d321fca9194f2185c91aa
Viktor Buyakov <buyakov_vv@soshace.com>
2017-05-14 22:17:36 +0300

    remove old

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,9 +29,8 @@ export class MyApp {
 
     // set our app's pages
     this.pages = [
-      { title: 'Словари', component: DictionaryPage},
-      { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'My First List', component: ListPage },
+      { title: 'Словари', component: DictionaryPage}
+     
 
     ];
   }
